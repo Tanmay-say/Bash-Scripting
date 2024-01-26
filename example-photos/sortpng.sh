23a01e4596dd385a9672d404335595bbe9726570
Tanmay Sayare <tanmaysayare12345@gmail.com>
2024-01-26 17:40:37 +0530

    added filter to txt also

--- example-photos/sortpng.sh
+++ example-photos/sortpng.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Print contents of the current directory sorted by last modification time
-ls -lt | grep -E '\.pdf$|\.png$|\.jpeg$'
+ls -lt | grep -E '\.txt$|\.pdf$|\.png$|\.jpeg$'
 
