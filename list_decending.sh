15a8c67cb832e5eb78d681ca014df8d6a0ca4467
Tanmay Sayare <tanmaysayare12345@gmail.com>
2024-01-26 17:33:10 +0530

    Script to reverse the order

---
+++ list_decending.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#!/bin/bash
+
+# Script to print contents of the current directory using ls command, sorted by name in descending order
+
+echo "Contents of the current directory, sorted by name (descending):"
+ls -1 | sort -r
+
